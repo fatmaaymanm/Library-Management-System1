b57c9fc955c02c34a24c5b61fc2086c8a02eeb51
fatmaaymanm <fatma.aymanm@gmail.com>
2023-04-17 14:57:01 +0200

    Methods added to Person class

--- src/Person.java
+++ src/Person.java
@@ -21,5 +21,17 @@ public abstract class Person {
         Email = email;
         this.isBlocked = isBlocked;
     }
+
+    public void registration() {
+
+    }
+
+    public void login() {
+
+    }
+
+    public void logout() {
+
+    }
 }
 
